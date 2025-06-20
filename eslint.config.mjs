cc1c4019a38115038dc9aa638985a2903d4bc2c9
Heath Babb <hbabb@heath-babb.com>
2025-06-19 20:26:07 -0400

    project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    type: 'app',
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: 'single',
+      bracketSameLine: true,
+      bracketSpacing: true,
+      endOfLine: 'lf',
+      arrowParens: 'avoid',
+      htmlWhiteSpaceSensitivity: 'css',
+      jsxSingleQuote: false,
+      quoteProps: 'as-needed',
+    },
+    ignore: [
+      '**/.pnpm-store/**',
+      '**/dist/**',
+    ],
+  },
+
+  {
+    rules: {
+      'ts/no-redeclare': 'off',
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'no-console': ['warn'],
+      'antfu/no-top-level-await': ['off'],
+      'node/prefer-global/process': ['off'],
+      'node/no-process-env': ['error'],
+      'perfectionist/sort-imports': ['error', {
+        tsconfigRootDir: '.',
+      }],
+      'unicorn/filename-case': ['warn', {
+        case: 'camelCase',
+        ignore: ['README.md', 'TODO.md', 'vite.config.ts', 'commitlint.config.js'],
+      }],
+    },
+  },
+  {
+    files: ['src/components/ui/*'],
+    rules: {
+      'unicorn/filename-case': 'off',
+    },
+  },
+)
