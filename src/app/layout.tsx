2c53cb53fe70d2c741711c0b6074e889ec7d55e8
Heath Babb <hbabb@heath-babb.com>
2025-06-26 13:06:33 -0400

    feat: added theme-provider from shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ import React from "react";
 
 import { Toaster } from "@/components/ui/sonner";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/providers/themeProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -164,22 +165,24 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className={cn("flex min-h-dvh flex-col", inter.className)}>
-        <main>{children}</main>
-        <Toaster richColors />
-        {/* @ts-expect-error - GoogleAnalytics is a valid component */}
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
-        <Script
-          id="clarity-script"
-          strategy="afterInteractive"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
+        <ThemeProvider>
+          <main>{children}</main>
+          <Toaster richColors />
+          {/* @ts-expect-error - GoogleAnalytics is a valid component */}
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+          <Script
+            id="clarity-script"
+            strategy="afterInteractive"
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `(function(c,l,a,r,i,t,y){
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
                 })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");`,
-          }}
-        />
+            }}
+          />
+        </ThemeProvider>
       </body>
     </html>
   );
